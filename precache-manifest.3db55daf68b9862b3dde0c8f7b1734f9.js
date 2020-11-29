a70bded8bf66d99f7dc55159d66aefbe1b0e6c08
GerritHoskins <gerrit.hoskins@gmail.com>
2020-11-29 14:55:52 +0100

    Updates

---
+++ precache-manifest.3db55daf68b9862b3dde0c8f7b1734f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f441d977f4980bca4f1c94aa981cf5c",
+    "url": "/react-blog-github/index.html"
+  },
+  {
+    "revision": "b6aba141d3f6766e1d8c",
+    "url": "/react-blog-github/static/css/main.1e18207e.chunk.css"
+  },
+  {
+    "revision": "0a7985e46fed9228a084",
+    "url": "/react-blog-github/static/js/2.a26dc40d.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/react-blog-github/static/js/2.a26dc40d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6aba141d3f6766e1d8c",
+    "url": "/react-blog-github/static/js/main.59befc5c.chunk.js"
+  },
+  {
+    "revision": "8216af6db86cb58257df",
+    "url": "/react-blog-github/static/js/runtime-main.b7e97252.js"
+  }
+]);
